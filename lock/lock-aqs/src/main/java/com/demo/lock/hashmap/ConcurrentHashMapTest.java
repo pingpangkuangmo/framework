c90a94eca8fd600a9f6875743247e0a22f54e1b1
ligang <917312290@qq.com>
2015-09-05 23:32:46 +0800

    ConcurrentHashMap开始分析

---
+++ lock/lock-aqs/src/main/java/com/demo/lock/hashmap/ConcurrentHashMapTest.java
@@ -0,0 +1,15 @@
+package com.demo.lock.hashmap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.Test;
+
+public class ConcurrentHashMapTest {
+	
+	private ConcurrentHashMap<String,String> map=new ConcurrentHashMap<String,String>();
+
+	@Test
+	public void testPut(){
+		map.put("key1","value1");
+	}
+}
