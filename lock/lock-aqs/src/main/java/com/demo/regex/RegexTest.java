d8151d56810944f0707c09323fc4e008ebf8af4a
lg李刚 <917312290@qq.com>
2015-09-06 19:40:42 +0800

    regex 贪婪模式、勉强模式、独占模式

--- lock/lock-aqs/src/main/java/com/demo/regex/RegexTest.java
+++ lock/lock-aqs/src/main/java/com/demo/regex/RegexTest.java
@@ -76,7 +76,7 @@ public class RegexTest {
         str = str.replaceAll("\"(.*?)\"", "@"); //勉强匹配模式    aaa@ccc@eee
         System.out.println(str);  
 	}
-	
+
 	@Test
 	public void testNoCapture(){
 		Pattern p = Pattern.compile("(?<!c)a(\\d+)bd");
@@ -97,5 +97,51 @@ public class RegexTest {
 		  System.out.println(m.group(1)); // 我们只要捕获组1的数字即可。结果 4343
 		}
 		System.out.println("over");
+	}
+	
+	@Test
+	public void testNoCapture3(){
+		Pattern p = Pattern.compile("(?:c)a(\\d+)bd");
+		Matcher m = p.matcher("da12bka3434bdca4343bdca234bm");
+		while(m.find()){
+		  System.out.println(m.group(0)); // 0组是整个表达式，看这里，但提炼出(?:c)的字符 。结果 ca4343bd
+		  System.out.println(m.group(1)); // 我们只要捕获组1的数字即可。结果 4343
+		}
+		System.out.println("over");
+	}
+	
+	@Test
+	public void testNoCapture4(){
+		Pattern pattern = Pattern.compile("(?:(\\d+))?\\s?([a-zA-Z]+)?.+");
+        String source = "2133 fdsdee4333";
+        Matcher matcher = pattern.matcher(source);
+        if(matcher.matches()){
+            for(int i=0;i<=matcher.groupCount();i++){
+                System.out.println("group "+i+":"+matcher.group(i));
+            }
+        }
+        System.out.println("over");
+	}
+	
+	@Test
+	public void testNoCapture5(){
+		String s = "xxyyxxxyxxyxx";  
+        Pattern greedy = Pattern.compile("xx(.*)xx");  
+        Pattern reluctant = Pattern.compile("xx(.*?)xx");  
+        Pattern possessive = Pattern.compile("xx(.*+)xx");  
+        Matcher m1 = greedy.matcher(s);   
+        Matcher m2 = reluctant.matcher(s);   
+        Matcher m3 = possessive.matcher(s);   
+        while(m1.find()) {  
+            System.out.println("greedy..." + m1.group(1));  
+        } 
+        System.out.println();
+        while(m2.find()) {  
+            System.out.println("reluctant..." + m2.group(1));  
+        } 
+        System.out.println();
+        while(m3.find()) {  
+            System.out.println("possessive..." + m3.group(1));  
+        }  
 	}
 }
