1e8ecfdf15e14f09bff87b90e01ec1b53c04424e
lg李刚 <917312290@qq.com>
2015-10-14 21:02:25 +0800

    update aop class

--- AOP/spring-aop/src/test/java/com/demo/test/dao/MainTest.java
+++ AOP/spring-aop/src/test/java/com/demo/test/dao/MainTest.java
@@ -1,8 +1,26 @@
 package com.demo.test.dao;
 
+import java.lang.reflect.InvocationHandler;
+
+import org.aopalliance.aop.Advice;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.aop.BeforeAdvice;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.framework.adapter.MethodBeforeAdviceInterceptor;
+import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
 
 public class MainTest {
+	
+	ProxyFactory proxyFactory;
+	ProxyFactoryBean proxyFactoryBean;
+	Pointcut pointcut;
+	Advice advice;
+	BeanNameAutoProxyCreator beanNameAutoProxyCreator;
+	InvocationHandler h;
+	BeforeAdvice beforeAdvice;
+	MethodBeforeAdviceInterceptor methodBeforeAdviceInterceptor;
 
 	@Before(value = "")
 	public void test(){
