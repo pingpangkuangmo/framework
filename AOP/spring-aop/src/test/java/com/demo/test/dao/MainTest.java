624cc96dfe67cea5dfe1dd9be9a15c7e3df0aaa6
ligang <917312290@qq.com>
2015-10-15 09:02:02 +0800

    update

--- AOP/spring-aop/src/test/java/com/demo/test/dao/MainTest.java
+++ AOP/spring-aop/src/test/java/com/demo/test/dao/MainTest.java
@@ -1,16 +1,22 @@
 package com.demo.test.dao;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.reflect.InvocationHandler;
 
 import org.aopalliance.aop.Advice;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.Pointcut;
+import org.springframework.aop.aspectj.autoproxy.AspectJAwareAdvisorAutoProxyCreator;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.framework.adapter.MethodBeforeAdviceInterceptor;
+import org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator;
 import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
+import org.springframework.beans.factory.xml.NamespaceHandler;
 
+@Aspect
 public class MainTest {
 	
 	ProxyFactory proxyFactory;
@@ -21,7 +27,13 @@ public class MainTest {
 	InvocationHandler h;
 	BeforeAdvice beforeAdvice;
 	MethodBeforeAdviceInterceptor methodBeforeAdviceInterceptor;
+	AbstractAutoProxyCreator abstractAutoProxyCreator;
+	AspectJAwareAdvisorAutoProxyCreator aspectJAwareAdvisorAutoProxyCreator;
+	NamespaceHandler namespaceHandler;
+	ClassFileTransformer classFileTransformer;
+	
 
+	@org.aspectj.lang.annotation.Pointcut
 	@Before(value = "")
 	public void test(){
 		
