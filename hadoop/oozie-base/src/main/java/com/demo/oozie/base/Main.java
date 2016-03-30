3acda5b41b32ac6d714ed5d4eaab6ba8cd40679e
ligang <917312290@qq.com>
2016-03-30 14:04:21 +0800

    update

--- hadoop/oozie-base/src/main/java/com/demo/oozie/base/Main.java
+++ hadoop/oozie-base/src/main/java/com/demo/oozie/base/Main.java
@@ -1,6 +1,16 @@
 package com.demo.oozie.base;
 
+import org.apache.oozie.command.wf.SubmitXCommand;
+import org.apache.oozie.service.WorkflowAppService;
+import org.apache.oozie.store.Store;
+import org.apache.oozie.workflow.lite.DBLiteWorkflowLib;
+import org.apache.oozie.workflow.lite.LiteWorkflowInstance;
+
 public class Main {
 
-	
+	WorkflowAppService workflowAppService;
+	SubmitXCommand submitXCommand;
+	LiteWorkflowInstance liteWorkflowInstance;
+	DBLiteWorkflowLib dbLiteWorkflowLib;
+	Store store;
 }
