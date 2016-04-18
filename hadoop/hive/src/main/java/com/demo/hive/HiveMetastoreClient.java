0e8b40dbd4fa7201c7eb7b8a46899050e90196e0
ligang <917312290@qq.com>
2016-04-18 18:05:03 +0800

    update

--- hadoop/hive/src/main/java/com/demo/hive/HiveMetastoreClient.java
+++ hadoop/hive/src/main/java/com/demo/hive/HiveMetastoreClient.java
@@ -1,25 +1,28 @@
 package com.demo.hive;
 
-import java.util.HashMap;
-
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
-import org.apache.hadoop.hive.metastore.api.AlreadyExistsException;
 import org.apache.hadoop.hive.metastore.api.Database;
-import org.apache.hadoop.hive.metastore.api.InvalidObjectException;
 import org.apache.hadoop.hive.metastore.api.MetaException;
-import org.apache.thrift.TException;
+import org.apache.hadoop.security.UserGroupInformation.HadoopLoginModule;
 import org.junit.Before;
 import org.junit.Test;
+
+import sun.util.logging.resources.logging;
 
 public class HiveMetastoreClient {
 	
 	HiveMetaStoreClient metaStoreClient = null;
+	
+	HadoopLoginModule loginModule;
+	//HadoopGroupMappingService hadoopGroupMappingService;
 
 	@Before
 	public void init(){
 		HiveConf conf = new HiveConf();
+		
 		try {
+			System.setProperty("HADOOP_USER_NAME", "admin");
 			metaStoreClient = new HiveMetaStoreClient(conf);
 		} catch (MetaException e) {
 			e.printStackTrace();
@@ -27,8 +30,11 @@ public class HiveMetastoreClient {
 	}
 	
 	@Test
-	public void createDb() throws AlreadyExistsException, InvalidObjectException, MetaException, TException{
-		metaStoreClient.createDatabase(new Database("test", "created by metaStoreClient", "hdfs://user/hive/warehouse/test", new HashMap<String, String>()));
+	public void createDb() throws Exception{
+		Database database = new Database();
+		database.setName("test_lg");
+		database.setOwnerName("lg123");
+		metaStoreClient.createDatabase(database);
 	}
 
 }
