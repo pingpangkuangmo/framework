3fd41472a65ada28251aefa642c1fa727d69939b
ligang <917312290@qq.com>
2016-04-25 08:18:03 +0800

    tomcat架构分析和HDFS权限验证

---
+++ hadoop/yarn/src/main/java/com/demo/yarn/HDFSMain.java
@@ -0,0 +1,13 @@
+package com.demo.yarn;
+
+import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.hdfs.protocol.ClientProtocol;
+import org.apache.hadoop.hdfs.server.namenode.NameNode;
+
+public class HDFSMain {
+
+	NameNode nameNode;
+	DFSClient dfsClient;
+	ClientProtocol clientProtocol;
+	//NameNodeRpcServer nameNodeRpcServer;
+}
