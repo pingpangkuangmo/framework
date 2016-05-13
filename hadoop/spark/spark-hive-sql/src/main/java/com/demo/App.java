830fb2ec824abdacec4819d274c5ab6c2f1c2cb9
ligang <917312290@qq.com>
2016-05-13 18:00:22 +0800

    研究HiveContext和Hive的客户端解析代码

---
+++ hadoop/spark/spark-hive-sql/src/main/java/com/demo/App.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import org.apache.spark.sql.hive.HiveContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    org.apache.spark.sql.hive.thriftserver.SparkSQLCLIDriver d;
+
+    static HiveContext hiveContext;
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        hiveContext.sql("");
+        hiveContext.table("");
+    }
+}
