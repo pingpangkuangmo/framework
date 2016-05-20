70666e7c346e8880a40291afc754332d9a34eb7b
ligang <917312290@qq.com>
2016-05-20 17:30:04 +0800

    update

--- hadoop/hive/src/main/java/com/demo/hive/Main.java
+++ hadoop/hive/src/main/java/com/demo/hive/Main.java
@@ -4,14 +4,16 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 import org.apache.hadoop.hive.metastore.HiveMetaStore;
+import org.apache.hadoop.hive.ql.parse.authorization.HiveAuthorizationTaskFactory;
 
 public class Main {
 
 	HiveMetaStore hiveMetaStore;
-	
-	                    
+
+	HiveAuthorizationTaskFactory hiveAuthorizationTaskFactory;
+
 	public static void main(String[] args) throws UnsupportedEncodingException{
-		System.out.println(URLDecoder.decode("hdfs://ns%2Fuser%2Fadmin%2FSearchAge", "UTF-8"));
+
 	}
 }
                                                                                                         
