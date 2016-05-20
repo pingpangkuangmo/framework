401bf9eb604e6c0c6f8e5f3e3efac21e1d76ffad
ligang <917312290@qq.com>
2016-05-20 17:59:50 +0800

    update

--- hadoop/hive/src/main/java/com/demo/hive/Main.java
+++ hadoop/hive/src/main/java/com/demo/hive/Main.java
@@ -4,12 +4,19 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 import org.apache.hadoop.hive.metastore.HiveMetaStore;
+import org.apache.hadoop.hive.ql.parse.AbstractSemanticAnalyzerHook;
+import org.apache.hadoop.hive.ql.parse.authorization.HiveAuthorizationTaskFactoryImpl;
+import org.apache.hive.service.server.HiveServer2;
 
 public class Main {
 
 	HiveMetaStore hiveMetaStore;
-	
-	                    
+
+	HiveServer2 hiveServer2;
+
+	HiveAuthorizationTaskFactoryImpl hiveAuthorizationTaskFactory;
+	AbstractSemanticAnalyzerHook hook;
+
 	public static void main(String[] args) throws UnsupportedEncodingException{
 		System.out.println(URLDecoder.decode("hdfs://ns%2Fuser%2Fadmin%2FSearchAge", "UTF-8"));
 	}
--- hadoop/spark/spark-hive-sql/src/main/java/com/demo/App.java
+++ hadoop/spark/spark-hive-sql/src/main/java/com/demo/App.java
@@ -1,5 +1,6 @@
 package com.demo;
 
+import org.apache.hive.service.cli.thrift.ThriftCLIService;
 import org.apache.spark.sql.hive.HiveContext;
 
 /**
@@ -10,6 +11,8 @@ public class App {
 
     org.apache.spark.sql.hive.thriftserver.SparkSQLCLIDriver d;
     org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 hiveThriftServer2;
+
+    ThriftCLIService thriftCLIService;
 
     static HiveContext hiveContext;
 
