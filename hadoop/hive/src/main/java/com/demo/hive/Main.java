c92888f6342d785fcc875549c84decf77ff99c41
ligang <917312290@qq.com>
2016-05-23 20:40:48 +0800

    update

--- hadoop/hive/src/main/java/com/demo/hive/Main.java
+++ hadoop/hive/src/main/java/com/demo/hive/Main.java
@@ -1,12 +1,22 @@
 package com.demo.hive;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 
 import org.apache.hadoop.hive.metastore.HiveMetaStore;
+import org.apache.hadoop.hive.metastore.ObjectStore;
+import org.apache.hadoop.hive.ql.Driver;
+import org.apache.hadoop.hive.ql.HiveDriverRunHook;
 import org.apache.hadoop.hive.ql.parse.AbstractSemanticAnalyzerHook;
 import org.apache.hadoop.hive.ql.parse.HiveSemanticAnalyzerHook;
 import org.apache.hadoop.hive.ql.parse.authorization.HiveAuthorizationTaskFactoryImpl;
+import org.apache.hadoop.hive.ql.security.HadoopDefaultAuthenticator;
+import org.apache.hadoop.hive.ql.security.SessionStateUserAuthenticator;
+import org.apache.hadoop.hive.ql.security.authorization.DefaultHiveAuthorizationProvider;
+import org.apache.hadoop.hive.ql.security.authorization.DefaultHiveMetastoreAuthorizationProvider;
+import org.apache.hadoop.hive.ql.security.authorization.HiveAuthorizationProvider;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthorizer;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthorizerFactory;
+import org.apache.hive.service.cli.session.HiveSessionHook;
 import org.apache.hive.service.server.HiveServer2;
 
 public class Main {
@@ -18,7 +28,29 @@ public class Main {
 	HiveAuthorizationTaskFactoryImpl hiveAuthorizationTaskFactory;
 	AbstractSemanticAnalyzerHook hook;
 	HiveSemanticAnalyzerHook hiveSemanticAnalyzerHook;
-
+	
+	HiveAuthorizationProvider hiveAuthorizationProvider;
+	Driver driver;
+	
+	HiveAuthorizationTaskFactoryImpl hiveAuthorizationTaskFactoryImpl;
+	
+	HiveAuthorizer hiveAuthorizer;
+	
+	HiveAuthorizerFactory hiveAuthorizerFactory;
+	
+	HiveSessionHook hiveSessionHook;
+	
+	ObjectStore objectStore;
+	
+	SessionStateUserAuthenticator sessionStateUserAuthenticator;
+	
+	DefaultHiveAuthorizationProvider defaultHiveAuthorizationProvider;
+	DefaultHiveMetastoreAuthorizationProvider defaultHiveMetastoreAuthorizationProvider;
+	
+	HadoopDefaultAuthenticator hadoopDefaultAuthenticator;
+	
+	HiveDriverRunHook hiveDriverRunHook;
+	
 	public static void main(String[] args) throws UnsupportedEncodingException{
 
 	}
