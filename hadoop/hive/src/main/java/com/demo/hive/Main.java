e3cc517eb52cef177e86281822b1aecddfa2ada4
ligang <917312290@qq.com>
2016-05-25 09:12:32 +0800

    update

--- hadoop/hive/src/main/java/com/demo/hive/Main.java
+++ hadoop/hive/src/main/java/com/demo/hive/Main.java
@@ -1,10 +1,18 @@
 package com.demo.hive;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaStore;
+import org.apache.hadoop.hive.metastore.HiveMetaStore.HMSHandler;
+import org.apache.hadoop.hive.metastore.MetaStorePreEventListener;
+import org.apache.hadoop.hive.metastore.ObjectStore;
+import org.apache.hadoop.hive.ql.Driver;
+import org.apache.hadoop.hive.ql.hooks.PostExecute;
 import org.apache.hadoop.hive.ql.parse.authorization.HiveAuthorizationTaskFactory;
+import org.apache.hadoop.hive.ql.parse.authorization.HiveAuthorizationTaskFactoryImpl;
+import org.apache.hive.service.cli.session.HiveSessionHook;
+import org.apache.hive.service.server.HiveServer2;
 
 public class Main {
 
@@ -12,6 +20,22 @@ public class Main {
 
 	HiveAuthorizationTaskFactory hiveAuthorizationTaskFactory;
 
+	Driver driver;
+	PostExecute postExecute;
+	
+	HiveConf hiveConf;
+	
+	HiveSessionHook hiveSessionHook;
+	
+	HMSHandler hmsHandler;
+	
+	MetaStorePreEventListener metaStorePreEventListener;
+	
+	HiveServer2 hiveServer2;
+	
+	HiveAuthorizationTaskFactoryImpl hiveAuthorizationTaskFactoryImpl;
+	
+	ObjectStore objectStore;
 	public static void main(String[] args) throws UnsupportedEncodingException{
 
 	}
