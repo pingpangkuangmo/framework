368a9521bda1b29d4f0a8cd1f7ab052d63a34793
ligang <917312290@qq.com>
2016-06-03 17:50:15 +0800

    update

---
+++ nio/thrift/thrift-base/src/main/java/com/demo/thrift/base/Main.java
@@ -0,0 +1,11 @@
+package com.demo.thrift.base;
+
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+
+public class Main {
+
+	TThreadPoolServer tThreadPoolServer;
+	TServer tServer;
+	
+}
