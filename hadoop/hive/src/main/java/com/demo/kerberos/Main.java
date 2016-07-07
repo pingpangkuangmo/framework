8362dc1792d28438d97f2eccfa90db197659ead9
ligang <917312290@qq.com>
2016-07-08 07:05:54 +0800

    update

--- hadoop/hive/src/main/java/com/demo/kerberos/Main.java
+++ hadoop/hive/src/main/java/com/demo/kerberos/Main.java
@@ -3,15 +3,25 @@ package com.demo.kerberos;
 import javax.security.sasl.SaslClient;
 import javax.security.sasl.SaslServer;
 
+import org.apache.hadoop.hive.metastore.HiveMetaStore;
+import org.apache.hadoop.hive.metastore.ObjectStore;
+import org.apache.hadoop.hive.ql.metadata.SessionHiveMetaStoreClient;
 import org.apache.hadoop.ipc.RPC.Server;
 import org.apache.hadoop.security.UserGroupInformation;
 
 public class Main {
 
-	Krb5LoginModule krb5LoginModule;
+	//Krb5LoginModule krb5LoginModule;
 	UserGroupInformation ugi;
 	Server server;
 	
 	SaslServer saslServer;
 	SaslClient saslClient;
+	
+	ObjectStore asfsd;
+	
+	SessionHiveMetaStoreClient sessionHiveMetaStoreClient;
+	
+	HiveMetaStore hiveMetaStore;
+	
 }
